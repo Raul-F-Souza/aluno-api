d6ce321386a04917328158ebceb39d75be483a37
Raul Souza <raulsouza1654@gmail.com>
2025-01-29 21:41:13 -0300

    feat: Classe AlunoRepository

--- src/main/java/com/aluno_api/aluno_api/repository/AlunoRepository.java
+++ src/main/java/com/aluno_api/aluno_api/repository/AlunoRepository.java
@@ -1,5 +1,8 @@
 package com.aluno_api.aluno_api.repository;
 
-public class AlunoRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aluno_api.aluno_api.model.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
